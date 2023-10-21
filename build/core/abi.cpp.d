6141450e5d96381ce9ff7bd72dfc82e3ea4db7f9
CarlosR99 <jose.carlos.silva@correounivalle.edu.co>
2023-10-21 16:10:39 -0500

    first commit

---
+++ build/core/abi.cpp.d
@@ -0,0 +1,2 @@
+D:\Jose\Metodologia\Proyectos\arduino_proyect\build\core\abi.cpp.o: \
+ C:\Users\Sofia\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\abi.cpp
---
+++ build/core/wiring_pulse.S.d
@@ -0,0 +1,2 @@
+D:\Jose\Metodologia\Proyectos\arduino_proyect\build\core\wiring_pulse.S.o: \
+ C:\Users\Sofia\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\wiring_pulse.S
